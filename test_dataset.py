be31b29f678d5c6b1d8c86953fc0394c3df6d210
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-11 11:10:03 +0200

    Tested torch.utils.data.DataLoader on ArtistsPaintingsDataset

--- test_dataset.py
+++ test_dataset.py
@@ -1,10 +1,19 @@
 from torchvision import transforms, utils
 from create_dataset import ArtistsPaintingsDataset
+from torch.utils.data import Dataset, DataLoader
 
-transform = transforms.ToTensor()
-data = ArtistsPaintingsDataset(transform=transform)
+transform = transforms.Compose([transforms.CenterCrop(size=(64,64)),
+                                transforms.Grayscale(num_output_channels=3),
+                                transforms.ToTensor()])
+data = ArtistsPaintingsDataset(transform = transform)
 
 
 
-example = data[0:10]
-print(len(example[0]))
+#example = data[0:10]
+#print(example)
+
+data_loader = DataLoader(data, batch_size=1000)
+
+for i,sample in enumerate(data_loader):
+    print(sample[0][0].shape)
+    print(sample[0][1].shape)
