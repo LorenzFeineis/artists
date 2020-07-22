e2438706683e6d1f75e41c9408b63b946983893b
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-22 23:43:08 +0200

    For Cross validation on the server; no cuda specifications yet.

---
+++ cross_validation.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+import numpy as np
+from PIL import Image
+
+import torchvision
+from torchvision import transforms, utils
+
+from create_dataset import ArtistsPaintingsDataset
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+from NeuralNet import Net
+
+from sklearn.model_selection import cross_val_predict
+from skorch import NeuralNetClassifier
+from sklearn.model_selection import cross_val_score
+
+### Transforms the images from the Dataset into torch.tensors()
+transform = transforms.Compose([transforms.ToTensor()])
+
+### Indixes of the chosen artists in artists_changed.csv
+artists_idx = [8,13,15,20,30,31,32,46]
+
+### The Train Dataset
+train_data = ArtistsPaintingsDataset(rootdir="aug_images/", transform = transform, mode="Train", artists_idx=artists_idx)
+
+### adjusting the tensors for the cross validation
+x_cv, y_cv = train_data.samples, train_data.targets
+x_cv = torch.cat(x_cv, dim = 0).resize(len(y_cv),3,256,256)
+print(x_cv.shape)
+y_cv = torch.tensor(y_cv)
+print(y_cv.shape)
+
+### will be a list of ouputs from cross_val_score
+scores = []
+
+### (1e-5,... ,1e1)
+for lr in np.logspace(-5,1,7):
+    ### net is the classifier based on our architecture with changing learning rate
+    net = NeuralNetClassifier(Net(size= (256,256), num_classes = 8),
+                             max_epochs = 100,
+                             train_split=None,
+                             criterion = torch.nn.CrossEntropyLoss,
+                             optimizer = torch.optim.SGD,
+                             lr = lr,
+                             optimizer__momentum = 0.9,
+                             batch_size = 160)
+    ### score is a numpy area with the scores of the classifier on each of the
+    ### 10 cross validation set
+    score = cross_val_score(net, x_cv, y_cv, cv=10)
+    scores.append(scores)
+
+scores = np.array(scores)
+np.save("Cross_Validation_scores.npy", scores)
