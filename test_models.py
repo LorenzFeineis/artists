13d6f69905c00044a8abf47ce8e4cb1e9437246f
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 11:17:06 +0200

    script to determine performance of the models.

---
+++ test_models.py
@@ -0,0 +1,30 @@
+import torch
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision
+from torchvision import transforms, utils
+
+from create_dataset import ArtistsPaintingsDataset
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+
+transform = transforms.Compose([transforms.CenterCrop(size=(256,256)),
+                                transforms.ToTensor()])
+
+artists_idx = [8,15,20,30]
+
+train_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Train", artists_idx=artists_idx)
+test_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Test", artists_idx=artists_idx)
+print(len(train_data), "train images loaded.")
+print(len(test_data), "test images loaded.")
+
+train_loader = DataLoader(train_data)
+test_loader = DataLoader(test_data)
+
+model = torch.load("model_lr_e-4.pt")
+
+test_loss = np.load("test_loss_e-4.npy")
+train_loss = np.load("train_loss_e-4.npy")
