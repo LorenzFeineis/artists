6318c0897e74dc4fa76a561dc4f69db30a88b6a7
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 12:21:45 +0200

    Defining a function to determine the accuracies.

--- test_models.py
+++ test_models.py
@@ -14,24 +14,18 @@ from tqdm import tqdm
 transform = transforms.Compose([transforms.CenterCrop(size=(256,256)),
                                 transforms.ToTensor()])
 
-artists_idx = [8,15,20,30]
 
-#train_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Train", artists_idx=artists_idx)
-#test_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Test", artists_idx=artists_idx)
-#print(len(train_data), "train images loaded.")
-#print(len(test_data), "test images loaded.")
 
-#train_loader = DataLoader(train_data)
-#test_loader = DataLoader(test_data)
 
-model = torch.load("model_lr_e-4.pt")
 
-test_loss = np.load("test_loss_e-4.npy",allow_pickle=True)
-train_loss = np.load("train_loss_e-4.npy", allow_pickle=True)
+
 
 
 
 def plot_losses():
+    test_loss = np.load("test_loss_e-4.npy",allow_pickle=True)
+    train_loss = np.load("train_loss_e-4.npy", allow_pickle=True)
+
     fig, axis = plt.subplots(ncols = 2)
 
     axis[0].plot(test_loss)
@@ -40,6 +34,26 @@ def plot_losses():
     axis[1].set(title = "trainings loss",xlabel="epochs", ylabel="CELoss")
     plt.savefig("losses_e-4.png")
 
+def performance():
+    model = torch.load("model_lr_e-4.pt")
+    model.eval()
+
+    artists_idx = [8,15,20,30]
+
+    train_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Train", artists_idx=artists_idx)
+    test_data = ArtistsPaintingsDataset(root_dir="aug_images/", transform = transform, mode="Test", artists_idx=artists_idx)
+    print(len(train_data), "train images loaded.")
+    print(len(test_data), "test images loaded.")
+
+    train_loader = DataLoader(train_data)
+    test_loader = DataLoader(test_data)
+
+    train_labels = torch.tensor(train_data)
+    test_labels = torch.tensor(test_data)
+
+    dataiter = iter(train_loader)
+    images, labels = dataiter.next()
 
 if __name__ == "__main__":
-    plot_losses()
+    #plot_losses()
+    performance()
