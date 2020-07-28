38dd3d0bc9ff49e96612e70fe19f4325fc1f47bd
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 13:04:42 +0200

    another try

--- test_models.py
+++ test_models.py
@@ -50,7 +50,7 @@ def performance(lr="e-5"):
         test_output = model(x_test)
         print("output",test_output)
         print("labels",y_test.data)
-        accuracy = np.count_nonzero(test_output.cpu()-y_test.cpu())
+        accuracy = np.count_nonzero(test_output.cpu().detach().numpy()-y_test.cpu().detach().numpy())
     #dataiter = iter(test_loader)
     #images, labels = dataiter.next()
     #print(images.shape)
