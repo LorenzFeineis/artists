3181623f21948d6d10f22a7ecdd500f52d145929
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 13:32:51 +0200

    I am optimistic

--- test_models.py
+++ test_models.py
@@ -55,7 +55,7 @@ def performance(lr="e-5"):
         #print("labels",y_test.data)
         print(prediction)
         print(ground_truth)
-        if np.array([ground_truth==prediction]).all:
+        if ground_truth[0]==prediction
             accuracy += 1
         print(accuracy)
     print(accuracy/len(test_data))
