f3814d0af2ee007b66fe3d642730fa154911b2d4
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 12:28:13 +0200

    plot losses of lr e-5; compute output of lre-4 just for understanding

--- test_models.py
+++ test_models.py
@@ -23,8 +23,8 @@ transform = transforms.Compose([transforms.CenterCrop(size=(256,256)),
 
 
 def plot_losses():
-    test_loss = np.load("test_loss_e-4.npy",allow_pickle=True)
-    train_loss = np.load("train_loss_e-4.npy", allow_pickle=True)
+    test_loss = np.load("test_loss_e-5.npy",allow_pickle=True)
+    train_loss = np.load("train_loss_e-5.npy", allow_pickle=True)
 
     fig, axis = plt.subplots(ncols = 2)
 
@@ -32,7 +32,7 @@ def plot_losses():
     axis[0].set(title = "test loss",xlabel="epochs", ylabel="CELoss")
     axis[1].plot(train_loss)
     axis[1].set(title = "trainings loss",xlabel="epochs", ylabel="CELoss")
-    plt.savefig("losses_e-4.png")
+    plt.savefig("losses_e-5.png")
 
 def performance():
     model = torch.load("model_lr_e-4.pt")
@@ -53,6 +53,8 @@ def performance():
 
     dataiter = iter(test_loader)
     images, labels = dataiter.next()
+    test_output = model(images)
+    print(test_output.shape)
 
 if __name__ == "__main__":
     #plot_losses()
