32f1a14b330c5d5fe7a85ffb87a067a0c92eb073
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 15:53:27 +0200

    Creates to folders containing test and train data

---
+++ split_data.py
@@ -0,0 +1,36 @@
+### Create two folders one for training_images, one for test_images
+from PIL import Image
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+
+root_dir = "aug_images/"
+csv_file = "artists_changed.csv"
+
+
+artists_info = pd.read_csv(csv_file)
+
+artists_idx = [8,15,20,30]
+
+artists_ids = range(len(artists_idx))
+artists_names = [artists_info["name"][idx] for idx in artists_idx]
+artists2id = dict(zip(artists_names,artists_ids))
+
+image_list = []
+for name in artists2id.keys():
+    file_start = name.replace(" ","_")
+    for file in os.listdir(root_dir):
+        if file.startswith(file_start):
+            image_list.append(file)
+
+train_data, test_data = train_test_split(image_list, train_size = 0.9, random_state=42)
+
+for train_file in train_data:
+    img = Image.open(train_file)
+    img.save("train_set/{}".format(train_file))
+    img.close()
+
+for test_file in test_data:
+    img = Image.open(test_file)
+    img.save("test_set/{}".format(test_file))
+    img.close()
