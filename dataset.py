a188edf89109a4dd2dd839bbfbf13eeea64546e4
Lorenz <sf230@stud.uni-heidelberg.de>
2020-07-28 16:10:16 +0200

    New dataset class, that reads data from train_set/ and test_set/

---
+++ dataset.py
@@ -0,0 +1,65 @@
+import torch
+import os
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+
+class ArtistsPaintingsDataset(Dataset):
+
+    def __init__(self, transform=None, mode = "Train",artists_idx = [8,15,20,30]):
+        self.transform = transform
+        ###
+        ### dictionary keys: artists name as string, key: id in range(0,num_artists)
+        artists_ids = range(len(artists_idx))
+        artists_names = [self.artists_info["name"][idx] for idx in artists_idx]
+        self.artists2id = dict(zip(artists_names,artists_ids))
+
+        if mode == "Train":
+            self.root_dir = "train_set/"
+        elif mode == "Test":
+            self.root_dir = "test_/set"
+        else:
+            print("mode must be \"Train\" or \"Test\"")
+
+        self.image_list = []
+        for name in self.artists2id.keys():
+            file_start = name.replace(" ","_")
+            for file in os.listdir(self.root_dir):
+                if file.startswith(file_start):
+                    self.image_list.append(file)
+
+        self.samples = []
+        self.targets = []
+        print("Load data:")
+        for img_name in tqdm(self.image_list):
+            image = Image.open(self.root_dir+"/"+img_name).convert('RGB')
+            artist_list = img_name.split("_")
+            artist = artist_list[0]
+            for string in artist_list[1:-1]:
+                artist += " "+string
+            try:
+                artist_id = self.artists2id[artist]
+
+                sample = image
+                if self.transform:
+                    sample = self.transform(image)
+                image.close()
+                self.samples.append(sample)
+                self.targets.append(artist_id)
+            except KeyError:
+                pass
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self,idx):
+        if type(idx)!= slice:
+            targets = [self.targets[idx]]
+        else:
+            targets = self.targets[idx]
+        return self.samples[idx], targets
