eb04389dc82df81bcee4234f9a745c11830942ca
Lorenz <sf230@stud.uni-heidelberg.de>
2020-08-06 01:05:59 +0200

    added parser to enable setting source and target in the terminal

--- augmentate.py
+++ augmentate.py
@@ -1,4 +1,3 @@
-from augm import get_augmentation, rand_crops
 from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,79 +9,104 @@ import argparse
 import numpy as np
 import errno
 
-parser = argparse.ArgumentParser(description='arguments for classification')
-parser.add_argument('--aug_images', type=str, default='../aug_images', metavar='X', help='image folder')
+parser = argparse.ArgumentParser(description='arguments for augmentation')
+parser.add_argument('--source', type=str, default='../resized', metavar='X', help='image folder')
+parser.add_argument('--target_dir', type=str, default='../train_aug_images/', metavar='X', help='target folder')
 parser.add_argument('--scale', type=list, default=(256, 256), metavar='X', help='input layer size')
-parser.add_argument('--img_process', type=str, default='crop', metavar='X', help='determines how images may be processed')
-parser.add_argument('--crops', type=int, default=4, metavar='X', help='number of crops being taken if img_process = crop')
-parser.add_argument('--color_convert', type=str, default="RGB", metavar='X', help='number of crops being taken if img_process = crop')
-parser.add_argument('--percent_l2_norm', type=float, default=1, metavar='X', help='L2 Norm on generated images and origin. Only take this percent range')
+parser.add_argument('--crops', type=int, default=4, metavar='X', help='number of crops being taken')
+parser.add_argument('--color_convert', type=str, default="RGB", metavar='X', help='colorcode to convert images to')
 args = parser.parse_args()
 
-artists_info = pd.read_csv('artists.csv')
+def rand_crops(img, crops=4, scale=(256, 256)):
+    w, h = img.size
+    w_args, h_args = scale
+
+    w -= w_args
+    h -= w_args
+
+    rand_w = np.random.randint(0, w, crops )
+    rand_h = np.random.randint(0, h, crops)
+
+    crop_list = []
+    for width, height in zip(rand_w, rand_h):
+        crop_list.append(img.crop((width, height, (width + w_args), (height + h_args))))
+
+    return crop_list
 
 
-os.chdir('train_set') ### working directory is now /resized
-
-print(os.getcwd())
-
-print("# of pictures in folder train_set:", len(os.listdir()))
+def augmentate(source,target,crops,scale,color_convert):
+    artists_info = pd.read_csv('artists.csv')
 
 
-### Create list of artists
-artists = []
-for artist in artists_info["name"]:
-    artists.append(artist.replace("ü", "ue")) ### replace ü with u because of Dürer
+    os.chdir(source) ### Change working directory
 
-### Create dictionary with keys: artists, values: id
-### Create dictionary with keys: artists, values: id
-artist2id = dict(zip
-                 (artists,artists_info["id"]))
-id2artist = dict(zip
-                 (artists_info["id"], artists))
+    print(os.getcwd())
 
-index = 0
-for image in tqdm(os.listdir()):
-    index +=1
-    parts = image.split("_")
-    artist = parts[0]
-    for part in parts[1:-1]:
-        artist += " " + part
-    if artist[0:8] == "Albrecht":
-        artist = "Albrecht Duerer"
-    try:
-        image_jpg = Image.open(image)
-        image_jpg = image_jpg.convert('RGB')
-
-        w,h = image_jpg.size
-        w_crop, h_crop = args.scale
-        if w<=w_crop or h<=h_crop:
-            image_jpg = image_jpg.resize(args.scale)
-            image_list = [image_jpg]
-        else:
-            image_list = rand_crops(image_jpg)
+    print("# of pictures in source directory:", len(os.listdir()))
 
 
-        if args.color_convert == "RGB":
-            if image_jpg.getcolors() != None:
-                image_jpg = image_jpg.convert('RGB')
+    ### Create list of artists
+    artists = []
+    for artist in artists_info["name"]:
+        artists.append(artist.replace("ü", "ue")) ### replace ü with u because of Dürer
 
-        elif args.color_convert == "L":
-            if image_jpg.getcolors() == None:
-                image_jpg = image_jpg.convert('L')
+    ### Create dictionary with keys: artists, values: id
+    ### Create dictionary with keys: artists, values: id
+    artist2id = dict(zip
+                     (artists,artists_info["id"]))
+    id2artist = dict(zip
+                     (artists_info["id"], artists))
+
+    index = 0
+    for image in tqdm(os.listdir()):
+        index +=1
+        parts = image.split("_")
+        artist = parts[0]
+        for part in parts[1:-1]:
+            artist += " " + part
+        if artist[0:8] == "Albrecht":
+            artist = "Albrecht Duerer"
+        try:
+            image_jpg = Image.open(image)
+            image_jpg = image_jpg.convert('RGB')
+
+            w,h = image_jpg.size
+            w_crop, h_crop = scale
+            if w<=w_crop or h<=h_crop:
+                image_jpg = image_jpg.resize(scale)
+                image_list = [image_jpg]
+            else:
+                image_list = rand_crops(image_jpg,crops,scale)
+
+
+            if color_convert == "RGB":
+                if image_jpg.getcolors() != None:
+                    image_jpg = image_jpg.convert('RGB')
+
+            elif color_convert == "L":
+                if image_jpg.getcolors() == None:
+                    image_jpg = image_jpg.convert('L')
 
 
 
-        for img in image_list:
-            vertical = img.transpose(method=Image.FLIP_TOP_BOTTOM)
-            horizontal = img.transpose(method=Image.FLIP_LEFT_RIGHT)
-            name = artist.replace(" ", "_")
-            for save_it in (img,vertical,horizontal):
-                file_name = str(name) +"_"+ str(index) + ".jpg"
-                save_it.save("../train_aug_images/"+file_name)
-                index +=1
+            for img in image_list:
+                vertical = img.transpose(method=Image.FLIP_TOP_BOTTOM)
+                horizontal = img.transpose(method=Image.FLIP_LEFT_RIGHT)
+                name = artist.replace(" ", "_")
+                for save_it in (img,vertical,horizontal):
+                    file_name = str(name) +"_"+ str(index) + ".jpg"
+                    save_it.save(target+file_name)
+                    index +=1
 
-        image_jpg.close()
+            image_jpg.close()
 
-    except KeyError:
-        print(artist,"not found in dictionary")
+        except KeyError:
+            print(artist,"not found in dictionary")
+
+if __name__ == '__main__':
+    source = args.source
+    target = args.target_dir
+    crops = args.crops
+    scale = args.scale
+    color_convert = args.color_convert
+    augmentate(source,target,crops,scale,color_convert)
